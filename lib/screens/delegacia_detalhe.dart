1a44edbb88c030aa0edb4c523769035c5759c4cc
GrrMoura <geibison@hotmail.com>
2024-07-05 10:54:56 -0300

    Mudanças: melhorias na tela detahes delegacias

--- lib/screens/delegacia_detalhe.dart
+++ lib/screens/delegacia_detalhe.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: use_build_context_synchronously
 
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -13,205 +15,207 @@ import 'package:suporte_dti/utils/app_colors.dart';
 import 'package:suporte_dti/utils/app_dimens.dart';
 import 'package:suporte_dti/utils/app_name.dart';
 import 'package:suporte_dti/utils/app_styles.dart';
-import 'package:suporte_dti/utils/app_validator.dart';
 import 'package:suporte_dti/utils/snack_bar_generic.dart';
 import 'package:suporte_dti/viewModel/equipamento_view_model.dart';
 
-//
 class DelegaciaDetalhe extends StatefulWidget {
   final String? sigla;
-  final String? nome;
+  final String nome;
   final EquipamentoViewModel? model;
-  const DelegaciaDetalhe(
-      {this.sigla, this.model, super.key, required this.nome});
+
+  const DelegaciaDetalhe({
+    super.key,
+    this.sigla,
+    required this.nome,
+    this.model,
+  });
 
   @override
   State<DelegaciaDetalhe> createState() => _DelegaciaDetalheState();
 }
 
 class _DelegaciaDetalheState extends State<DelegaciaDetalhe> {
-  ScrollController? scrollController;
-  EquipamentoController equipamentoController = EquipamentoController();
+  final ScrollController _scrollController = ScrollController();
+  final EquipamentoController _equipamentoController = EquipamentoController();
+
+  late Future<void> _future;
+
+  bool _isLoading = false;
+  Map<int, bool> _isLoadingMap = {};
 
   @override
   void initState() {
-    setState(() {});
     super.initState();
-    scrollController = ScrollController()..addListener(_scrollListener);
+    _scrollController.addListener(_scrollListener);
+    _future = _fetchData();
   }
 
   @override
   void dispose() {
-    scrollController?.removeListener(_scrollListener);
-    scrollController?.dispose();
-    widget.model?.itensEquipamentoModels.equipamentos = [];
-    widget.model?.paginacao?.pagina = 1;
-    widget.model?.paginacao?.totalPaginas = 1;
+    _scrollController.removeListener(_scrollListener);
+    _scrollController.dispose();
     super.dispose();
   }
 
-  // Controle do scroll: ao final do scroll é carregado novos elementos
+  Future<void> _fetchData() async {
+    try {
+      await _equipamentoController.buscarEquipamentos(context, widget.model!);
+      setState(() {});
+    } catch (e) {
+      print("Erro ao buscar equipamentos: $e");
+    }
+  }
+
   void _scrollListener() {
-    if (scrollController?.position.pixels ==
-        scrollController?.position.maxScrollExtent) {
+    if (_scrollController.position.pixels ==
+        _scrollController.position.maxScrollExtent) {
       if (widget.model?.paginacao == null ||
           !widget.model!.paginacao!
               .seChegouAoFinalDaPagina(widget.model!.paginacao!.pagina!)) {
-        setState(() {});
-
-        equipamentoController
-            .buscarEquipamentos(context, widget.model!)
-            .then((value) {
-          setState(() {});
-        });
+        _fetchData();
       } else {
         Generic.snackBar(
-            context: context,
-            mensagem: "A lista chegou ao fim.",
-            tipo: AppName.info);
+          context: context,
+          mensagem: "A lista chegou ao fim.",
+          tipo: AppName.info,
+        );
       }
     }
   }
 
-  Widget _buildCard(ItemEquipamento itens) {
-    return InkWell(
-        onTap: () {
-          equipamentoController
-              .buscarEquipamentoPorId(context, itens)
-              .then((value) {
-            setState(() {});
-          });
-        },
-        child: CardEquipamentosResultado(item: itens));
-  }
+  Widget _buildCard(ItemEquipamento item, int index) {
+    bool isLoading = _isLoadingMap[index] ??
+        false; // Verifica se está carregando para o índice atual
 
-  int touchedIndex = -1;
+    return InkWell(
+      onTap: () async {
+        setState(() {
+          _isLoadingMap[index] =
+              true; // Ativa o indicador de carregamento para o índice atual
+        });
+
+        await _equipamentoController.buscarEquipamentoPorId(context, item);
+
+        setState(() {
+          _isLoadingMap[index] =
+              false; // Desativa o indicador de carregamento para o índice atual após a resposta
+        });
+      },
+      child:
+          isLoading ? LoadingDefault() : CardEquipamentosResultado(item: item),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    setState(() {});
-    FutureBuilder futureScreen() {
-      return FutureBuilder(
-          future:
-              equipamentoController.buscarEquipamentos(context, widget.model!),
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.done:
-                if (snapshot.hasError) {
-                  return Container(
-                    color: Colors.white,
-                  );
-                } else if (!snapshot.hasData ||
-                    (snapshot.data as List).isEmpty) {
-                  return _listViewScreen();
-                } else {
-                  return _listViewScreen();
-                }
-
-              default:
-                return const LoadingDefault();
-            }
-          });
-    }
-
-    return Validador.listNotNullAndNotEmpty(
-            widget.model?.itensEquipamentoModels.equipamentos)
-        ? _listViewScreen()
-        : futureScreen();
-  }
-
-  Widget _listViewScreen() {
-    double screenWidth = MediaQuery.of(context).size.height;
     return Scaffold(
-        appBar: AppBar(
-          iconTheme: const IconThemeData(color: Colors.white),
-          leading: IconButton(
-              onPressed: () {
-                widget.model?.paginacao?.pagina = 1;
-                widget.model?.paginacao?.totalPaginas = 1;
-                Navigator.of(context).pop();
-              },
-              icon: const Icon(Icons.arrow_back_ios_new)),
-          title: Padding(
-            padding: EdgeInsets.only(top: 10.h),
-            child: Text(widget.sigla ?? "",
-                style: Styles().titleStyle().copyWith(
-                    color: AppColors.cWhiteColor,
-                    fontSize: 22.sp,
-                    fontWeight: FontWeight.normal)),
-          ),
-          centerTitle: true,
-          backgroundColor: AppColors.cSecondaryColor,
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.white),
+        leading: IconButton(
+          onPressed: () {
+            widget.model?.paginacao?.pagina = 1;
+            widget.model?.paginacao?.totalPaginas = 1;
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back_ios_new),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
+        title: Padding(
+          padding: EdgeInsets.only(top: 10.h),
+          child: Text(
+            widget.sigla ?? "",
+            style: Styles().titleStyle().copyWith(
+                  color: AppColors.cWhiteColor,
+                  fontSize: 22.sp,
+                  fontWeight: FontWeight.normal,
+                ),
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: AppColors.cSecondaryColor,
+      ),
+      body: FutureBuilder(
+        future: _future,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(
+                child: Text("Erro ao carregar dados: ${snapshot.error}"));
+          } else {
+            return SingleChildScrollView(
+              child: Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: 250.w,
+                          child: Text(
+                            widget.nome,
+                            style: Styles().smallTextStyle(),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 16),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Titulos(nome: "  Levantamentos  "),
+                        AddBotao(),
+                      ],
+                    ),
                     SizedBox(
-                      width: 250.w,
-                      child:
-                          Text(widget.nome!, style: Styles().smallTextStyle()),
+                      height: 350.h,
+                      child: ListView(
+                        children: const [
+                          DelegaciasCardLevantamento(
+                            idUnidade: 1,
+                            nome: "Juan Matos Silva",
+                            id: "02",
+                            data: "12/10/2023",
+                            delegacia: "Resumo",
+                          ),
+                        ],
+                      ),
+                    ),
+                    Titulos(
+                      nome:
+                          " ${widget.model?.itensEquipamentoModels.equipamentos.length} de ${widget.model?.paginacao!.registros} equipamentos  ",
+                    ),
+                    SizedBox(
+                      height: 500.h,
+                      child: GridView.builder(
+                        physics: const BouncingScrollPhysics(
+                            parent: AlwaysScrollableScrollPhysics()),
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          mainAxisExtent: 220,
+                          mainAxisSpacing: 15,
+                          crossAxisSpacing: 15,
+                        ),
+                        controller: _scrollController,
+                        itemCount: widget
+                            .model?.itensEquipamentoModels.equipamentos.length,
+                        itemBuilder: (BuildContext ctxt, int index) {
+                          return _buildCard(
+                              widget.model!.itensEquipamentoModels
+                                  .equipamentos[index],
+                              index);
+                        },
+                      ),
                     ),
                   ],
                 ),
               ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Titulos(nome: "  Levantamentos  "),
-                  AddBotao(),
-                ],
-              ),
-              SizedBox(
-                height: 350.h,
-                child: ListView(
-                  children: [
-                    DelegaciasCardLevantamento(
-                        idUnidade: widget.model!.idUnidade!,
-                        nome: "Juan Matos Silva",
-                        id: "02",
-                        data: "12/10/2023",
-                        delegacia: "Resumo"),
-                  ],
-                ),
-              ),
-              widget.model?.itensEquipamentoModels.equipamentos.length != null
-                  ? Titulos(
-                      nome:
-                          " ${widget.model?.itensEquipamentoModels.equipamentos.length} de ${widget.model?.paginacao!.registros} equipamentos  ")
-                  : const Titulos(nome: "  Equipamentos "),
-              _blocoEquipamento(screenWidth),
-            ],
-          ),
-        ));
-  }
-
-  SizedBox _blocoEquipamento(double screenWidth) {
-    return SizedBox(
-      height: screenWidth - kToolbarHeight - 115.h,
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 5.h),
-        child: GridView.builder(
-          physics: const BouncingScrollPhysics(
-              parent: AlwaysScrollableScrollPhysics()),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              mainAxisExtent: 220,
-              mainAxisSpacing: 15,
-              crossAxisSpacing: 15),
-          controller: scrollController,
-          itemCount: widget.model?.itensEquipamentoModels.equipamentos.length,
-          itemBuilder: (BuildContext ctxt, int index) {
-            return _buildCard(
-                widget.model!.itensEquipamentoModels.equipamentos[index]);
-          },
-        ),
+            );
+          }
+        },
       ),
     );
   }
